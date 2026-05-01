7d73e0fe8a2a26cc4ad293dcc4336386129c10f2
DaltonTheDeveloper <dredden1293@outlook.com>
2026-04-30 23:46:38 -0500

    finished phase 1

--- src/model/Robot.cpp
+++ src/model/Robot.cpp
@@ -26,19 +26,23 @@ const std::vector<Point2D>& Robot::getHistory() const {
 }
 
 void Robot::moveUp() {
-    moveTo(x, y - 1);
+    int step = (moveCount >= upgradeThreshold) ? 2 : 1;
+    moveTo(x, y - step);
 }
 
 void Robot::moveDown() {
-    moveTo(x, y + 1);
+    int step = (moveCount >= upgradeThreshold) ? 2 : 1;
+    moveTo(x, y + step);
 }
 
 void Robot::moveLeft() {
-    moveTo(x - 1, y);
+    int step = (moveCount >= upgradeThreshold) ? 2 : 1;
+    moveTo(x - step, y);
 }
 
 void Robot::moveRight() {
-    moveTo(x + 1, y);
+    int step = (moveCount >= upgradeThreshold) ? 2 : 1;
+    moveTo(x + step, y);
 }
 
 void Robot::undo() {
@@ -114,6 +118,7 @@ void Robot::moveTo(int newX, int newY) {
 
     x = newX;
     y = newY;
+    moveCount++;
     recordPosition();
 }
 
